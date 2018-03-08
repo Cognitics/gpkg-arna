24222a0ba9f4be00ffe9778c437e2e080afa102d
kevinbentley <kevin.bentley@gmail.com>
2018-03-08 09:08:12 -0700

    Progress reading geopackage files.

--- projects/NavApp/app/src/main/java/net/cognitics/navapp/MainActivity.java
+++ projects/NavApp/app/src/main/java/net/cognitics/navapp/MainActivity.java
@@ -1,11 +1,15 @@
 package net.cognitics.navapp;
+import android.net.Uri;
 import android.view.View;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.io.File;
+import java.net.URL;
+import java.util.List;
 
 import mil.nga.geopackage.*;
 import mil.nga.geopackage.factory.GeoPackageFactory;
@@ -39,32 +43,43 @@ public class MainActivity extends AppCompatActivity {
                     public void onSelectedFilePaths(String[] files) {
                         //files is an array of the paths of files selected by the Application User.
                         if(files.length > 0) {
-                            openGeoPackage(files[0]);
+                            if(!openGeoPackage(files[0])); {
+                                AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(MainActivity.this);
+                                dlgAlert.setMessage("Unable to open " + files[0]);
+                                dlgAlert.setTitle("Error");
+                                dlgAlert.setPositiveButton("OK", null);
+                                dlgAlert.setCancelable(true);
+                                dlgAlert.create().show();
+                            }
                         }
                     }
                 });
                 dialog.show();
-                // Open a GeoPackage
-                //GeoPackageManager manager = GeoPackageFactory.getManager(this);
-                //gpkgDb = manager.open("");
+
             }
         });
 
     }
 
-    public void openGeoPackage(String path)
+    public Boolean openGeoPackage(String path)
     {
-        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
-        dlgAlert.setMessage("File selected: " + path);
-        dlgAlert.setTitle("Selected File");
-        dlgAlert.setPositiveButton("OK", null);
-        dlgAlert.setCancelable(true);
-        dlgAlert.create().show();
         // Open a GeoPackage
-        //GeoPackageManager manager = GeoPackageFactory.getManager(this);
-        //gpkgDb = manager.open("");
+        GeoPackageManager manager = GeoPackageFactory.getManager(this);
 
+        manager.importGeoPackage("test",new File(path));
+        gpkgDb = manager.open("test");
+        if(gpkgDb != null) {
+            StringBuilder message = new StringBuilder();
+            message.append("Tables:\n");
+            List<String> tables = gpkgDb.getFeatureTables();
+            for(String table : tables){
+                message.append(table).append("\n");
+            }
+            final EditText msgText = findViewById(R.id.messages);
+            msgText.setText(message.toString());
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
 
-        return;
     }
 }
