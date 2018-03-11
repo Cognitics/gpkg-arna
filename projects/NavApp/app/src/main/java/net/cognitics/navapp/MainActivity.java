e032b366aaab4284fc93f8628dc3f9eb4805575d
Chris <chris.bentley480@gmail.com>
2018-03-10 17:23:15 -0700

    Combined with my files

--- projects/NavApp/app/src/main/java/net/cognitics/navapp/MainActivity.java
+++ projects/NavApp/app/src/main/java/net/cognitics/navapp/MainActivity.java
@@ -1,5 +1,16 @@
 package net.cognitics.navapp;
-import net.cognitics.navapp.GPSTracker;
+
+import android.hardware.Camera;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.karan.churi.PermissionManager.PermissionManager;
+
+
+//import net.cognitics.navapp.GPSTracker;
 import android.view.View;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +19,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
@@ -29,56 +41,103 @@ import com.github.angads25.filepicker.model.DialogConfigs;
 import com.github.angads25.filepicker.model.DialogProperties;
 import com.github.angads25.filepicker.view.FilePickerDialog;
 
-import static java.lang.Boolean.*;
-import net.cognitics.navapp.GreatCircle;
+
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 public class MainActivity extends AppCompatActivity {
+
+    Camera camera;
+    FrameLayout cameraPreview;
+    ShowCamera showCamera;
+
+    PermissionManager permissionManager;
+
     private GeoPackage gpkgDb;
     GPSTracker gps;
 
     public MainActivity() {
+        //NULL
+    }
+
+    /*
+    * Runs on app launch
+    */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Basic app creation
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Ask for runtime permission
+        permissionManager = new PermissionManager() {};
+        permissionManager.checkAndRequestPermissions(this);
+
+        // Initalize Location tracker
+        gps = new GPSTracker(this);
+
+
+        // Get camera stuff
+        camera = Camera.open();
+        cameraPreview = (FrameLayout) findViewById(R.id.cameraPreview);
+        showCamera = new ShowCamera(this,camera);
+        cameraPreview.addView(showCamera);
+
+        //Bring constraint layout to front
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.Constraint);
+        constraintLayout.bringToFront();
+
 
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        gps = new GPSTracker(this);
-        final Button button = findViewById(R.id.openGpkg);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                DialogProperties properties = new DialogProperties();
-                properties.selection_mode = DialogConfigs.SINGLE_MODE;
-                properties.selection_type = DialogConfigs.FILE_SELECT;
-                properties.root = new File("/");//DialogConfigs.DEFAULT_DIR);
-                properties.error_dir = new File(DialogConfigs.DEFAULT_DIR);
-                properties.offset = new File(DialogConfigs.DEFAULT_DIR);
-                properties.extensions = null;
-                FilePickerDialog dialog = new FilePickerDialog(MainActivity.this,properties);
-                dialog.setTitle("Select a File");
-                dialog.setDialogSelectionListener(new DialogSelectionListener() {
-                    @Override
-                    public void onSelectedFilePaths(String[] files) {
-                        //files is an array of the paths of files selected by the Application User.
-                        if(files.length > 0) {
-                            if(!openGeoPackage(files[0])) {
-                                AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(MainActivity.this);
-                                dlgAlert.setMessage("Unable to open " + files[0]);
-                                dlgAlert.setTitle("Error");
-                                dlgAlert.setPositiveButton("OK", null);
-                                dlgAlert.setCancelable(true);
-                                dlgAlert.create().show();
-                            }
-                        }
-                    }
-                });
-                dialog.show();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionManager.checkResult(requestCode, permissions, grantResults);
+    }
 
+    /*
+    * Functionality for choosing gpkg from files
+    * @param v View
+     */
+    public void onClick(View v) {
+        DialogProperties properties = new DialogProperties();
+        properties.selection_mode = DialogConfigs.SINGLE_MODE;
+        properties.selection_type = DialogConfigs.FILE_SELECT;
+        properties.root = new File(DialogConfigs.DEFAULT_DIR);
+        properties.root = new File("/");//DialogConfigs.DEFAULT_DIR);
+        properties.error_dir = new File(DialogConfigs.DEFAULT_DIR);
+        properties.offset = new File(DialogConfigs.DEFAULT_DIR);
+        properties.extensions = null;
+        FilePickerDialog dialog = new FilePickerDialog(MainActivity.this,properties);
+        dialog.setTitle("Select a File");
+        dialog.setDialogSelectionListener(new DialogSelectionListener() {
+            @Override
+            public void onSelectedFilePaths(String[] files) {
+                //files is an array of the paths of files selected by the Application User.
+                if(files.length > 0) {
+                    if(!openGeoPackage(files[0])) {
+                        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(MainActivity.this);
+                        dlgAlert.setMessage("Unable to open " + files[0]);
+                        dlgAlert.setTitle("Error");
+                        dlgAlert.setPositiveButton("OK", null);
+                        dlgAlert.setCancelable(true);
+                        dlgAlert.create().show();
+                    }
+                }
             }
         });
+        dialog.show();
 
     }
+
+
+    /*
+    * Reads Geo Package
+    * @param path filepath for gpkg
+     */
 
     private Boolean openGeoPackage(String path)
     {
@@ -172,7 +231,7 @@ public class MainActivity extends AppCompatActivity {
                 message.append(distance);
                 message.append(" miles\n");
             }
-            final EditText msgText = findViewById(R.id.messages);
+            final EditText msgText = (EditText) findViewById(R.id.messages);
             msgText.setText(message.toString());
             return TRUE;
         }
@@ -181,4 +240,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+
 }
